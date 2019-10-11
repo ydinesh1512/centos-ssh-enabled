77e90b271ed025e992a439177181a6758d138c8a
thapakazi <milans.thapa78@gmail.com>
2019-10-11 10:13:15 +0545

    mock2 - syntax fix

--- mock2/bootstrap.sh
+++ mock2/bootstrap.sh
@@ -9,4 +9,4 @@ JQ_BIN=/usr/bin/jq JQ_VERSION=1.6\
 yum install -y python3-pip
 last_20th_version=$(get_latest_pip_pkg_versions.sh awscli|head -1) 
 version=${last_20th_version:-'1.16.238'} # this is last 20th release at 11 Oct 2019
-pip3 install -y awscli==$version
+pip3 install awscli==$version
